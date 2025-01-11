0e7046ef6cf5db330d42e643b7f4b8582cbd8135
bstephe9 <bstephenson9807@gmail.com>
2025-01-11 05:42:31 -0500

    test: Add test for roms read into CHIP-8 memory

--- chip-8/tests/test_chip8.c
+++ chip-8/tests/test_chip8.c
@@ -56,10 +56,18 @@ void test_should_get_correct_rom_file_size(void) {
     TEST_ASSERT_EQUAL(132, rom_size);
 }
 
+void test_should_read_rom_into_memory(void) {
+    unsigned char buf[MAX_ROM_SIZE];
+    read_rom(buf, rom_path);
+    read_rom(&chip8.memory[PC_START], rom_path);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(&buf, &chip8.memory[PC_START], 132);
+}
+
 // Change 'main' to 'SDL_main' to avoid conflict with SDL2's entry point
 int SDL_main(int argc, char *argv[]) {
     UNITY_BEGIN();
     RUN_TEST(test_should_zero_initialize);
     RUN_TEST(test_should_get_correct_rom_file_size);
+    RUN_TEST(test_should_read_rom_into_memory);
     return UNITY_END();
 }
