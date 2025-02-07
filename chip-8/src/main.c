0bc8f5054eac07d4016dcefbd23f683e5a84508c
bstephe9 <bstephenson9807@gmail.com>
2025-02-07 01:01:57 -0500

    feat: Cap frame rate at 60 FPS (16.67ms per frame)

--- chip-8/src/main.c
+++ chip-8/src/main.c
@@ -21,8 +21,18 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
 
     while (chip8.state == RUNNING) {
+        uint64_t start_time = SDL_GetPerformanceCounter();
+
         handle_input(&chip8);
-        emulate_cycle(&chip8);
+
+        // 11 instructions per frame = 660 instructions per second
+        for (size_t i = 0; i < 11; i++) {
+            emulate_cycle(&chip8);
+
+            // If draw instruction, break to only draw once during this frame
+            if ((chip8.opcode >> 12) == 0xD)
+                break;
+        }
 
         if (chip8.draw) {
             update_display(&chip8);
@@ -30,6 +40,16 @@ int main(int argc, char *argv[]) {
         }
 
         update_timers(&chip8);
+
+        uint64_t end_time = SDL_GetPerformanceCounter();
+        double elapsed_time =
+            (end_time - start_time) / (double)SDL_GetPerformanceFrequency();
+
+        // Delay for the remainder of this current frame
+        double delay_amount = 16.67f - elapsed_time;
+        if (delay_amount > 0) {
+            SDL_Delay(delay_amount);
+        }
     }
 
     cleanup(&chip8.sdl);
